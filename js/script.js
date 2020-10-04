fdc73e0bd770f78bda0403388c1040c36ee36a64
Deborah M Ingabire <ingabineza@gmail.com>
2020-10-04 13:04:00 +0200

    added hover effect

--- js/script.js
+++ js/script.js
@@ -11,120 +11,141 @@ function output(){
     
     $(document).ready(function(){
      $("#1, #one").click(function(){
-         $(".walrus-hidden1").toggle();
+         $(".hidden1").toggle();
          $("#one").toggle();
      });
     });
     
     $(document).ready(function(){
         $("#2, #two").click(function(){
-            $(".walrus-hidden2").toggle();
+            $(".hidden2").toggle();
             $("#two").toggle();
     
         });
     });
     $(document).ready(function(){
         $("#3, #three").click(function(){
-            $(".walrus-hidden3").toggle();
+            $(".hidden3").toggle();
             $("#three").toggle();
     
         });
     });
-    
-    
-        $('.word1').hide();
-        $('#w1').animate({opacity:1});
-    
-        $('#img1').hover(function(){
-            $(this).stop().animate({opacity:.4},200);
-            $('.word1').fadeIn();
-        },
-        function(){
-            $(this).stop().animate({opacity:1},500)
-            $('.word1').fadeOut();      
-        });
-        // ------------------------------------------
-    
-        $('.word2').hide();
-        $('#w2').animate({opacity:1});
-    
-        $('#img2').hover(function(){
-            $(this).stop().animate({opacity:.4},200);
-            $('.word2').fadeIn();
-        },
-        function(){
-            $(this).stop().animate({opacity:1},500)
-            $('.word2').fadeOut();      
-        });
-    // -----------------------------------------
-    $('.word3').hide();
-    $('#w3').animate({opacity:1});
-    
-    $('#img3').hover(function(){
-        $(this).stop().animate({opacity:.4},200);
-        $('.word3').fadeIn();
-    },
-    function(){
-        $(this).stop().animate({opacity:1},500)
-        $('.word3').fadeOut();      
-    });
-    //----------------------------------------- 
-    $('.word4').hide();
-    $('#w4').animate({opacity:1});
-    
-    $('#img4').hover(function(){
-        $(this).stop().animate({opacity:.4},200);
-        $('.word4').fadeIn();
-    },
-    function(){
-        $(this).stop().animate({opacity:1},500)
-        $('.word4').fadeOut();      
-    });
-    // -------------------------------------
-    $('.word5').hide();
-    $('#w5').animate({opacity:1});
-    
-    $('#img5').hover(function(){
-        $(this).stop().animate({opacity:.4},200);
-        $('.word5').fadeIn();
-    },
-    function(){
-        $(this).stop().animate({opacity:1},500)
-        $('.word5').fadeOut();      
-    });
-    // ------------------------------
-    $('.word6').hide();
-    $('#w6').animate({opacity:1});
-    
-    $('#img6').hover(function(){
-        $(this).stop().animate({opacity:.4},200);
-        $('.word6').fadeIn();
-    },
-    function(){
-        $(this).stop().animate({opacity:1},500)
-        $('.word6').fadeOut();      
-    });
-    // ---------------------------------
-    $('.word7').hide();
-    $('#w7').animate({opacity:1});
-    
-    $('#img7').hover(function(){
-        $(this).stop().animate({opacity:.4},200);
-        $('.word7').fadeIn();
-    },
-    function(){
-        $(this).stop().animate({opacity:1},500)
-        $('.word7').fadeOut();      
-    });
-    // ------------------------------
-    $('.word8').hide();
-    $('#w8').animate({opacity:1});
-    
-    $('#img8').hover(function(){
-        $(this).stop().animate({opacity:.4},200);
-        $('.word8').fadeIn();
-    },
-    function(){
-        $(this).stop().animate({opacity:1},500)
-        $('.word8').fadeOut();      
-    });
+
+
+
+        $(document).ready(function(){
+            $('#pro1').hide();
+
+            $("#w1").hover(function(){
+              $(this).animate({opacity: .8}, 100);
+              $("#pro1").fadeIn();
+
+            },
+            function(){
+              $(this).stop().animate({opacity:1},400);
+              $('#pro1').fadeOut();  
+            });
+          });
+
+
+          $(document).ready(function(){
+            $('#pro2').hide();
+
+            $("#w2").hover(function(){
+              $(this).animate({opacity: .6}, 100);
+              $("#pro2").fadeIn();
+
+            },
+            function(){
+              $(this).stop().animate({opacity:1},400);
+              $('#pro2').fadeOut();  
+            });
+          });
+
+
+          $(document).ready(function(){
+            $('#pro3').hide();
+
+            $("#w3").hover(function(){
+              $(this).animate({opacity: .6}, 100);
+              $("#pro3").fadeIn();
+
+            },
+            function(){
+              $(this).stop().animate({opacity:1},400);
+              $('#pro3').fadeOut();  
+            });
+          });
+
+
+          $(document).ready(function(){
+            $('#pro4').hide();
+
+            $("#w4").hover(function(){
+              $(this).animate({opacity: .6}, 100);
+              $("#pro4").fadeIn();
+
+            },
+            function(){
+              $(this).stop().animate({opacity:1},400);
+              $('#pro4').fadeOut();  
+            });
+          });
+
+
+          $(document).ready(function(){
+            $('#pro5').hide();
+
+            $("#w5").hover(function(){
+              $(this).animate({opacity: .6}, 100);
+              $("#pro5").fadeIn();
+
+            },
+            function(){
+              $(this).stop().animate({opacity:1},400);
+              $('#pro5').fadeOut();  
+            });
+          });
+
+          $(document).ready(function(){
+            $('#pro6').hide();
+
+            $("#w6").hover(function(){
+              $(this).animate({opacity: .6}, 100);
+              $("#pro6").fadeIn();
+
+            },
+            function(){
+              $(this).stop().animate({opacity:1},400);
+              $('#pro6').fadeOut();  
+            });
+          });
+
+          $(document).ready(function(){
+            $('#pro7').hide();
+
+            $("#w7").hover(function(){
+              $(this).animate({opacity: .8}, 100);
+              $("#pro7").fadeIn();
+
+            },
+            function(){
+              $(this).stop().animate({opacity:1},400);
+              $('#pro7').fadeOut();  
+            });
+          });
+
+          $(document).ready(function(){
+            $('#pro8').hide();
+
+            $("#w8").hover(function(){
+              $(this).animate({opacity: .8}, 100);
+              $("#pro8").fadeIn();
+
+            },
+            function(){
+              $(this).stop().animate({opacity:1},400);
+              $('#pro8').fadeOut();  
+            });
+          });
+
